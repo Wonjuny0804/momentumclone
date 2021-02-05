c32c9a490957505dc794c78e76028ce9c1852b1c
WonjunJang <wonjun84@naver.com>
2021-02-05 16:34:48 +0900

    feat: Create codes for quote section

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
-import "@babel/polyfill";
-import backgroundRender from './background';
+// import "@babel/polyfill";
+import backgroundRender from './background.js';
+import todayFocus from './todayFocus.js';
+import quote from './quote.js'
 
 backgroundRender();
+todayFocus();
+quote();
