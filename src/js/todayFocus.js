69a77109a89fc8a5a6185562832390ea3cf55f68
WonjunJang <wonjun84@naver.com>
2021-02-05 14:01:11 +0900

    feat: Complete javascript code with DOM manipulation

---
+++ src/js/todayFocus.js
@@ -0,0 +1,40 @@
+const todayFocus = () => {
+  // DOM elements
+  const $focusWrapper = document.querySelector('.today-focus-wrapper');
+  const $focusInput = document.querySelector('.today-focus-input');
+  const $todoWrapper = document.querySelector('.focus-todo-wrapper');
+  const $focusTodo = document.querySelector('.focus-check-wrapper > label');
+  const $focusChkBox = document.getElementById('todayFocus');
+  
+  const toggleActive = () => {
+    $focusWrapper.classList.toggle('active');
+    $focusWrapper.classList.toggle('not-active');
+    $todoWrapper.classList.toggle('active');
+    $todoWrapper.classList.toggle('not-active');
+  }
+
+  $focusInput.addEventListener('keyup', e => {
+    if (e.key !== 'Enter') return;
+    toggleActive();
+    $focusTodo.textContent = e.target.value;
+  });
+  
+  $todoWrapper.addEventListener('click', e => {
+    console.log(e.target);
+    if (!e.target.classList.contains('remove-today-focus')) return;
+    toggleActive();
+    $focusInput.focus();
+    $focusInput.value = '';
+  });
+
+  $focusChkBox.addEventListener('change', e => {
+    console.log(e.target.checked);
+    if (e.target.checked) $focusTodo.classList.toggle('finished');
+    else $focusTodo.classList.toggle('finished');
+  })
+
+}
+
+todayFocus();
+ 
+export default todayFocus;
