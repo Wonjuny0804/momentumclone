381d013cc6cb4e826d45902fd5915859be877889
WonjunJang <wonjun84@naver.com>
2021-02-05 17:58:07 +0900

    feat:Complete all files

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,8 @@ import "@babel/polyfill";
 import backgroundRender from './background';
 import todayFocus from './todayFocus.js';
 import weatherInit from './weather';
-import quote from './quote.js'
+import quote from './quote.js';
+import '../css/style.css';
 
 backgroundRender();
 weatherInit();
